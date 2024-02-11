0bf9a91e19a2e4593db79d9c2ff96e8f08286e27
bennyhinnmathew <benny.mathew@student.moringaschool.com>
2024-02-11 09:51:31 +0300

    done

---
+++ migrations/versions/4216015b6a05_empty_init.py
@@ -0,0 +1,24 @@
+"""Empty Init
+
+Revision ID: 4216015b6a05
+Revises: 
+Create Date: 2024-02-11 09:34:55.860244
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '4216015b6a05'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
